35fd0e5c1432d04b387beedca2d5585464eba007
joferarma <78570783+joferarma@users.noreply.github.com>
2025-06-15 21:26:19 -0600

    Tarea 7
    Terminada

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                brand: '#ff5733',
+            },
+            fontFamily: {
+                poppins: ['Poppins', 'sans-serif']
+            },
+            spacing: {
+                2025: '5.06rem'
+            },
+            screens: {
+                xs: '400px'
+            },
+            width: {
+                'custom-w': '30rem'
+            },
+            minWidth: {
+                'custom-min-w': '20rem'
+            },
+            minHeight: {
+                'custom-min-h': '10rem'
+            },
+        },
+    }
+}
